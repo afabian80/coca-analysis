425729894ac7b82848e3cf33f62101765138bb66
Akos Fabian <fabian.akos@gmail.com>
2015-12-12 10:53:18 +0100

    Massive refactor, using many small methods (clean code)

--- src/main/groovy/hu/afabian/coca/Main.groovy
+++ src/main/groovy/hu/afabian/coca/Main.groovy
@@ -12,6 +12,24 @@ import groovyx.gpars.extra166y.Ops
 
 
 class Main {
+	private File inputFile
+	private Map cocaSetMap
+	private String outputDirName
+	private Map inputKiloBlocks
+	private Set inputSet
+	private int inputSize
+	private Set unknownSet
+	private Double percentageSoFar
+	private File outputDir
+
+	Main() {
+		this.cocaSetMap = [:]
+		this.inputKiloBlocks = [:]
+		this.outputDirName = 'coca-analysis-output'
+		this.percentageSoFar = 0.0
+		outputDir = new File(this.outputDirName)
+	}
+
 	static void main(String[] args) {
 		def start = System.currentTimeMillis()
 
@@ -21,75 +39,39 @@ class Main {
 		}
 
 		Main main = new Main()
+		main.setInputFile(new File(args[0]))
+		main.loadCocaDatabase()
+		main.collectInputWords()
+		main.partitionInputSet()
+		main.saveAnalysisOutput()
+
+		def end = System.currentTimeMillis()
+		def duration = end - start
+		println "Execution took $duration ms"
+	}
+
+	void setInputFile(inputFile) {
+		this.inputFile = inputFile
+	}
+
+	void loadCocaDatabase() {
 		File cocaDir = new File(this.getClass().getResource('/cocadb').getFile())
-		def cocaSetMap = [:]
-		def outputDirName = 'output'
 
 		print "Processing coca files "
 		cocaDir.eachFileRecurse(FileType.FILES) { cocaFile ->
 			def matcher = (cocaFile.name =~ "basewrd(.*).txt")
 			String actualSet = matcher[0][1]
-			//println "Processing coca ${actualSet}-k database..."
 			print "."
-			cocaSetMap[actualSet] = main.individualWords(cocaFile.text)
+			this.cocaSetMap[actualSet] = individualWords(cocaFile.text)
 		}
 		println " done."
+	}
 
-		File file = new File(args[0])
-		String inputText = file.text
-
+	void collectInputWords() {
 		println "Collecting words from input file..."
-
-		Set inputSet = main.individualWords(inputText)
-		Integer inputWords = inputSet.size()
-		println "Number of words in input: $inputWords"
-
-		Double percentageSoFar = 0.0
-
-		def inputKBlocks = [:]
-		cocaSetMap.each { key, value ->
-			inputKBlocks[key] = inputSet.intersect(value)
-
-			Integer blockWords = inputKBlocks[key].size()
-			String blockWordsText = sprintf('%8d', blockWords)
-
-			Double percentage = 100.0 * (double) blockWords / inputWords
-			String percentageText = sprintf('%6.2f', percentage)
-
-			percentageSoFar += percentage
-			String percentageSoFarText = sprintf('%6.2f', percentageSoFar)
-
-			println "Number of k-${key} words: $blockWordsText ($percentageText %  -> $percentageSoFarText %)"
-		}
-
-		def unknownSet = inputSet
-		inputKBlocks.each { key, value ->
-			unknownSet = unknownSet.minus(inputKBlocks[key])
-		}
-		Integer unknownWords = unknownSet.size()
-		Double percentage = 100.0 * (double) unknownWords / inputWords
-		String percentageText = sprintf('%6.2f', percentage)
-		println "Number of words not in database: $unknownWords ($percentageText %)"
-
-		println "Saving words to '$outputDirName' directory..."
-		def outputDir = new File(outputDirName)
-		if(outputDir.exists()) {
-			outputDir.deleteDir()
-		}
-		outputDir.mkdirs()
-
-		inputKBlocks.each { key, value ->
-			def blockFile = new File(outputDir, "k-${key}.txt")
-			blockFile.text = value.join('\n')
-		}
-
-		def unknownFile = new File(outputDir, "off-the-list.txt")
-		unknownFile.text = unknownSet.join('\n')
-
-		def end = System.currentTimeMillis()
-		def duration = end - start
-		println "Execution took $duration ms"
-
+		this.inputSet = individualWords(this.inputFile.text)
+		this.inputSize = inputSet.size()
+		println "Number of words in input: ${this.inputSize}"
 	}
 
 	Set individualWords(String text) {
@@ -99,6 +81,62 @@ class Main {
 			def sorted = collected.sort()
 			return collected
 		}
+	}
+
+	void partitionInputSet() {
+		this.cocaSetMap.each { key, value ->
+			registerInputBlock(key, value)
+		}
+		registerUnknownBlock()
+	}
+
+	void registerInputBlock(key, value) {
+		def block = this.inputSet.intersect(value)
+		this.inputKiloBlocks[key] = block
+		printBlockStats(block, key)
+	}
+
+	void registerUnknownBlock() {
+		this.unknownSet = this.inputSet
+		this.inputKiloBlocks.each { key, value ->
+			this.unknownSet = this.unknownSet.minus(this.inputKiloBlocks[key])
+		}
+		printBlockStats(this.unknownSet, '??')
+	}
+
+	void printBlockStats(block, key) {
+		Integer blockSize = block.size()
+		String blockSizeText = sprintf('%8d', blockSize)
+
+		Double percentage = 100.0 * (double) blockSize / (this.inputSize)
+		String percentageText = sprintf('%6.2f', percentage)
+
+		this.percentageSoFar += percentage
+		String percentageSoFarText = sprintf('%6.2f', percentageSoFar)
+
+		println "Number of k-${key} words: $blockSizeText ($percentageText %  -> $percentageSoFarText %)"
+	}
+
+	void saveAnalysisOutput() {
+		println "Saving words to '${this.outputDir}' directory..."
+		ensureCleanOutputDir(outputDir)
+
+		this.inputKiloBlocks.each { key, value ->
+			writeBlockToFile(key, value)
+		}
+		writeBlockToFile('unknown', unknownSet)
+	}
+
+	void ensureCleanOutputDir(dir) {
+		if(outputDir.exists()) {
+			outputDir.deleteDir()
+		}
+		outputDir.mkdirs()
+	}
+
+	void writeBlockToFile(key, value) {
+		def blockFile = new File(outputDir, "k-${key}.txt")
+		blockFile.text = value.join('\n')
 	}
 
 }
